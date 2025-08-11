e2674af0d83c674781c1e4d61c563b5f90b2a8d8
Angelina <angelina03wu@gmail.com>
2025-08-10 21:27:22 -0400

    when will this end

--- core/__init__.py
+++ core/__init__.py
@@ -3,4 +3,8 @@ from mongoengine import connect
 from dotenv import load_dotenv
 
 load_dotenv()
-connect(host=os.getenv("MONGODB_URI"))
+connect(
+	host=os.getenv("MONGODB_URI"),
+	tls=True,
+	tlsAllowInvalidCertificates=True  # Only for debugging! Remove in production if possible.
+)
