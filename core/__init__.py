9b59ff677e3109db2c4ed7bcded76fb33f6756c8
Angelina <angelina03wu@gmail.com>
2025-08-10 21:30:14 -0400

    aaaaaaaaaa

--- core/__init__.py
+++ core/__init__.py
@@ -1,8 +1,9 @@
 import os
-from mongoengine import connect
+from mongoengine import connect, disconnect
 from dotenv import load_dotenv
 
 load_dotenv()
+disconnect()  # Disconnect any previous default connection
 connect(
 	host=os.getenv("MONGODB_URI"),
 	tls=True,
